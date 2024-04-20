0c02a00d60f3e94a462606bdc98040c47c38107a
Dima Lipin <dimich3d@ya.ru>
2024-04-20 18:15:09 +0200

    better input control

--- frontend/src/components/Input.svelte
+++ frontend/src/components/Input.svelte
@@ -1,14 +1,25 @@
 <script lang="ts">
 	import { send } from '$lib/chat';
-	import { Input } from 'svelte-ux';
+	import { Button, Input } from 'svelte-ux';
 
 	let input = '';
 </script>
 
-<div class="p-2 border rounded-xl my-2 mx-4">
+<div class="flex p-2 border rounded-xl my-2 mx-4">
 	<Input
 		placeholder="Type your message here"
 		bind:value={input}
-		on:keyup={(e) => e.key === 'Enter' && send(input)}
+		on:keyup={(e) => {
+			if (e.ctrlKey && e.key === 'Enter') {
+				send(input);
+			}
+		}}
+	/>
+
+	<Button
+		icon="m2 21l21-9L2 3v7l15 2l-15 2z"
+		on:click={() => send(input)}
+		class="p-2"
+		target="_blank"
 	/>
 </div>
