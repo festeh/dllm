6f23618374977b2a8dd2dcee2084fa64a6a129c6
Dima Lipin <dimich3d@ya.ru>
2024-04-20 21:01:19 +0200

    resizable input

--- frontend/src/components/Input.svelte
+++ frontend/src/components/Input.svelte
@@ -1,18 +1,20 @@
 <script lang="ts">
 	import { send, stopChat } from '$lib/chat';
-	import { Button, Input } from 'svelte-ux';
+	import { Button } from 'svelte-ux';
 	import { isChatIdle } from '../stores/chatState';
-  import { mdiStop } from '@mdi/js';
 	import { mdiStopCircleOutline } from '@mdi/js';
+	import { resizeOutputField } from '$lib';
 
 	let input = '';
 </script>
 
 <div class="flex p-2 border rounded-xl my-2 mx-4">
-	<Input
+	<textarea
+		class="flex-1 bg-transparent outline-none resize-none p-2"
 		placeholder="Type your message here"
 		bind:value={input}
 		on:keyup={(e) => {
+			resizeOutputField(e.target);
 			if (e.ctrlKey && e.key === 'Enter') {
 				send(input);
 			}
@@ -22,10 +24,6 @@
 	{#if $isChatIdle}
 		<Button icon="m2 21l21-9L2 3v7l15 2l-15 2z" on:click={() => send(input)} class="p-2" />
 	{:else}
-		<!-- hand-stop -->
-		<Button
-			icon={mdiStopCircleOutline}
-			on:click={stopChat}
-		/>
+		<Button icon={mdiStopCircleOutline} on:click={stopChat} />
 	{/if}
 </div>
