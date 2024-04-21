e65d0db43a7e5c6c789cbe50ac71f21e605aa73c
Dima Lipin <dimich3d@ya.ru>
2024-04-21 23:41:25 +0200

    start android plugin

--- frontend/android/app/src/main/java/dllm/app/MainActivity.java
+++ frontend/android/app/src/main/java/dllm/app/MainActivity.java
@@ -1,5 +1,13 @@
 package dllm.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import dllm.app.plugins.Client.ClientPlugin;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(ClientPlugin.class);
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ frontend/android/app/src/main/java/dllm/app/plugins/Client/ClientPlugin.java
@@ -0,0 +1,12 @@
+package dllm.app.plugins.Client;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+
+@CapacitorPlugin(name = "Client")
+public class ClientPlugin extends Plugin {
+
+}
