2e45621cb39385222b0d41f1428db9bad140662d
Dmitrii Lipin <dimich3d@ya.ru>
2024-04-21 12:29:02 +0200

    add capacitor

---
+++ frontend/android/app/src/main/java/dllm/app/MainActivity.java
@@ -0,0 +1,5 @@
+package dllm.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,6 +1,5 @@
-import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-
+import adapter from '@sveltejs/adapter-static';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -8,10 +7,10 @@ const config = {
   preprocess: [vitePreprocess({})],
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: adapter({
+      pages: 'dist',
+      assets: 'dist',
+    }),
   },
 };
 
