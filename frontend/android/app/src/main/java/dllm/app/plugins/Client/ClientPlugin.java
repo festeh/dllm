56d19d5a4e4de9fd4029efbf325ce004be504ab4
Dima Lipin <dimich3d@ya.ru>
2024-04-28 13:52:38 +0200

    upd

--- frontend/android/app/src/main/java/dllm/app/plugins/Client/ClientPlugin.java
+++ frontend/android/app/src/main/java/dllm/app/plugins/Client/ClientPlugin.java
@@ -8,5 +8,10 @@ import com.getcapacitor.annotation.CapacitorPlugin;
 
 @CapacitorPlugin(name = "Client")
 public class ClientPlugin extends Plugin {
+    @PluginMethod(returnType = PluginMethod.RETURN_CALLBACK)
+    public void send(PluginCall call) {
+        System.out.println("Sending message to server");
+        call.setKeepAlive(true);
 
+    }
 }
